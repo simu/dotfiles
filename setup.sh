eec3a7ddede2bce58a17934b2bcfe7b92fa72b5b
Simon Gerber <simon.gerber@inf.ethz.ch>
2016-10-14 11:41:01 +0200

    setup.sh: fix installing puppet if not available, install required puppet modules

--- setup.sh
+++ setup.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 which puppet
-if [ $? -ne 0 -o ! -e /opt/puppetlabs/bin/puppet ]; then
+if [ $? -ne 0 -a ! -e /opt/puppetlabs/bin/puppet ]; then
 	wget https://apt.puppetlabs.com/puppetlabs-release-pc1-`lsb_release -c -s`.deb -O puppetlabs-release-repo.deb
 	sudo dpkg -i puppetlabs-release-repo.deb
+	rm puppetlabs-release-repo.deb
 	sudo apt update
 	sudo apt install puppet-agent
+	sudo /opt/puppetlabs/bin/puppet module install  --target-dir=/opt/puppetlabs/puppet/modules --modulepath /etc/puppetlabs/code/modules puppetlabs-vcsrepo
+	sudo /opt/puppetlabs/bin/puppet module install --target-dir=/opt/puppetlabs/puppet/modules --modulepath /etc/puppetlabs/code/modules wtanaka-mkdir
 fi
 sudo apt install git python-mpd python-jinja2
 
