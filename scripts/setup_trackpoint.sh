3f31b1bfe8251360e04f6d442dc501d556f88d2f
Simon Gerber <simon.gerber@inf.ethz.ch>
2017-10-13 10:22:15 +0200

    Fix X1 tablet trackpoint setup script to not rely on hard-coded xinput device ids

--- scripts/setup_trackpoint.sh
+++ scripts/setup_trackpoint.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
+# vim:et:sw=4
+
+possible_ids=$(xinput list | awk '/ThinkPad.*slave\s+pointer/ { split($9,xinp,"=");print xinp[2] }')
 
 # figure out if trackpoint is xinput id 12 or 13:
 # Trackpoint has button middle and touchpad doesn't
-xinput list-props 12 | grep "Button Middle" >/dev/null
-if [ $? -eq 0 ]; then
-	TP_DEVICE=12
-	# disable touchpad
-	xinput disable 13
-else
-	xinput list-props 13 | grep "Button Middle" >/dev/null
-	if [ $? -eq 0 ]; then
-		TP_DEVICE=13
-		# disable touchpad
-		xinput disable 12
-	fi
-fi
+for id in $possible_ids; do
+    xinput list-props $id | grep "Button Middle" >/dev/null
+    if [ $? -eq 0 ]; then
+        TP_DEVICE=$id
+    else
+        # disable touchpad
+        xinput disable $id
+    fi
+done
 devinput=$(xinput list-props $TP_DEVICE | grep "Device Node" | cut -d\" -f2)
 echo "TrackPoint is Device $TP_DEVICE ($devinput)"
 
