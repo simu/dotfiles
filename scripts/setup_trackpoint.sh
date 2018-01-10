66a1248330c6bb3ecd16b24fc1477728f03cd103
Simon Gerber <simon.gerber@inf.ethz.ch>
2018-01-10 11:03:18 +0100

    Update setup_trackpoint.sh to work when trackpad detected as synaptics

--- scripts/setup_trackpoint.sh
+++ scripts/setup_trackpoint.sh
@@ -1,22 +1,31 @@
 #!/bin/sh
 # vim:et:sw=4
 
-possible_ids=$(xinput list | awk '/ThinkPad.*slave\s+pointer/ { split($9,xinp,"=");print xinp[2] }')
+xinput list | grep "Synaptics" 2>&1 >/dev/null
+# if no synaptics device do old-style stuff
+if [ $? -ne 0 ]; then
 
-# figure out if trackpoint is xinput id 12 or 13:
-# Trackpoint has button middle and touchpad doesn't
-for id in $possible_ids; do
-    xinput list-props $id | grep "Button Middle" >/dev/null
-    if [ $? -eq 0 ]; then
-        TP_DEVICE=$id
-    else
-        # disable touchpad
-        xinput disable $id
-    fi
-done
-devinput=$(xinput list-props $TP_DEVICE | grep "Device Node" | cut -d\" -f2)
-echo "TrackPoint is Device $TP_DEVICE ($devinput)"
+    possible_ids=$(xinput list | awk '/ThinkPad.*slave\s+pointer/ { split($9,xinp,"=");print xinp[2] }')
 
-xinput enable $TP_DEVICE
-xinput set-prop $TP_DEVICE "Evdev Wheel Emulation" 1
-xinput set-prop $TP_DEVICE "Evdev Wheel Emulation Button" 2
+    # figure out if trackpoint is xinput id 12 or 13:
+    # Trackpoint has button middle and touchpad doesn't
+    for id in $possible_ids; do
+        xinput list-props $id | grep "Button Middle" >/dev/null
+        if [ $? -eq 0 ]; then
+            TP_DEVICE=$id
+        else
+            # disable touchpad
+            xinput disable $id
+        fi
+    done
+    devinput=$(xinput list-props $TP_DEVICE | grep "Device Node" | cut -d\" -f2)
+    echo "TrackPoint is Device $TP_DEVICE ($devinput)"
+
+    xinput enable $TP_DEVICE
+    xinput set-prop $TP_DEVICE "Evdev Wheel Emulation" 1
+    xinput set-prop $TP_DEVICE "Evdev Wheel Emulation Button" 2
+
+else
+    # use synclient to disable touchpad. Trackpoint just works(tm)
+    synclient TouchpadOff=1
+fi
