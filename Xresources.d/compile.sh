440838bd2d2388267e1b9942820aec86524b7b08
Simon Gerber <simon.gerber@vshn.ch>
2019-01-15 13:08:38 +0100

    Improve Xresources handling, allow machine-specific snippets

---
+++ Xresources.d/compile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -x
+
+DATADIR=`dirname $0`
+
+siteconfig=`find $DATADIR/parts.d -name "${HOSTNAME}.*"`
+
+OUTFILE=$CONFDIR/Xresources
+
+cat $DATADIR/Xresources > $OUTFILE
+for f in $siteconfig; do
+    cat $f >> $OUTFILE
+done
