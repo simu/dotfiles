1b7085ecac363e86cf5182e71039f65c528c4056
Simon Gerber <simon.gerber@vshn.ch>
2019-02-05 10:05:48 +0100

    scripts/next_wallpaper.sh: Fix log message

--- scripts/next_wallpaper.sh
+++ scripts/next_wallpaper.sh
@@ -3,5 +3,5 @@ files=(~/Pictures/wallpapers/*)
 WALLPAPER=$(printf "%s\n" "${files[RANDOM % ${#files[@]}]}")
 WALLPAPER2=$(printf "%s\n" "${files[RANDOM % ${#files[@]}]}")
 WALLPAPER3=$(printf "%s\n" "${files[RANDOM % ${#files[@]}]}")
-echo "[`date`] Changing wallpapers to $WALLPAPER, $WALLPAPER2" >> /tmp/next_wp.log
+echo "[`date`] Changing wallpapers to $WALLPAPER, $WALLPAPER2 $WALLPAPER3" >> /tmp/next_wp.log
 feh --bg-fill $WALLPAPER --bg-fill $WALLPAPER2 --bg-fill $WALLPAPER3
