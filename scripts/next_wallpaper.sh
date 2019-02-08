362b407f6b1d7857394fd9535575348af1248ee0
Simon Gerber <simon.gerber@vshn.ch>
2019-02-08 10:07:24 +0100

    Fix wallpaper switching

--- scripts/next_wallpaper.sh
+++ scripts/next_wallpaper.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+export DISPLAY=$1
 files=(~/Pictures/wallpapers/*)
 WALLPAPER=$(printf "%s\n" "${files[RANDOM % ${#files[@]}]}")
 WALLPAPER2=$(printf "%s\n" "${files[RANDOM % ${#files[@]}]}")
 WALLPAPER3=$(printf "%s\n" "${files[RANDOM % ${#files[@]}]}")
-echo "[`date`] Changing wallpapers to $WALLPAPER, $WALLPAPER2 $WALLPAPER3" >> /tmp/next_wp.log
-feh --bg-fill $WALLPAPER --bg-fill $WALLPAPER2 --bg-fill $WALLPAPER3
+echo "[$(date)] Changing wallpapers to $WALLPAPER, $WALLPAPER2 $WALLPAPER3" >> /tmp/next_wp.log
+feh --bg-fill "$WALLPAPER" --bg-fill "$WALLPAPER2" --bg-fill "$WALLPAPER3" >> /tmp/next_wp.log 2>&1
