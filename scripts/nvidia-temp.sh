8985bfe5d0c744eff2e27d0412f9034419436b7a
Simon Gerber <simon.gerber@vshn.ch>
2021-11-17 17:56:14 +0100

    Assorted changes on phoenix
    Mostly relating to the CPU/MB switch

--- scripts/nvidia-temp.sh
+++ scripts/nvidia-temp.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -eo pipefail
+set -x
 
 echo "Reading Nvidia temp"
 readonly temp=$(nvidia-smi -q -d TEMPERATURE | awk '/GPU Current/{ print $5 };')
---
+++ scripts/x570-temp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -eo pipefail
+
+echo "Reading Ryzen/X570 temp"
+readonly temp=$(sensors -j | jq -r '."nct6798-isa-0290".SYSTIN.temp1_input * 1000')
+readonly liquid_temp=$(echo "$(liquidctl status | grep 'Liquid temperature' | tr -s ' ' | cut -d' ' -f4) * 1000" | bc | cut -d. -f1)
+
+echo "${temp}" > /tmp/cputemp
+echo "${liquid_temp}" > /tmp/liquidtemp
