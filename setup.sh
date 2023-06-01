1bf8592ea985fa501c111bd9ab95751db23a349f
Simon Gerber <simon.gerber@vshn.ch>
2023-06-01 14:12:05 +0200

    Improve setup.sh script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-which puppet
-if [ $? -ne 0 -a ! -e /opt/puppetlabs/bin/puppet ]; then
-	wget https://apt.puppetlabs.com/puppet-release-`lsb_release -c -s`.deb -O puppetlabs-release-repo.deb
+set -eo pipefail
+
+if [ ! -e /opt/puppetlabs/bin/puppet ]; then
+	wget https://apt.puppetlabs.com/puppet7-release-`lsb_release -c -s`.deb -O puppetlabs-release-repo.deb
 	sudo dpkg -i puppetlabs-release-repo.deb
 	rm puppetlabs-release-repo.deb
 	sudo apt update
@@ -11,6 +12,7 @@ if [ $? -ne 0 -a ! -e /opt/puppetlabs/bin/puppet ]; then
 	sudo /opt/puppetlabs/bin/puppet module install --target-dir=/opt/puppetlabs/puppet/modules --modulepath /etc/puppetlabs/code/modules wtanaka-mkdir
 fi
 sudo apt install git python3-mpd python3-jinja2
+pip install pulsectl
 
 mkdir -p $(dirname "$0")/generated
 python3 generate_manifest.py
